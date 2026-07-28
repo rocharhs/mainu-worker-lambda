f4945bf4190ef75aa3e378a6ae93e086ac6189ad
rocharhs-admin-at-013803789137 <rocharhs@gmail.com>
2026-07-28 18:04:57 -0300

    remove codigo twilio e volta para uma lambda simples que apenas faz o log de que chegou mensagem

--- worker.py
+++ worker.py
@@ -1,12 +1,10 @@
 import json
 from datetime import datetime, timezone, timedelta
 import logging
-import requests
 import boto3
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Attr
 
-from twilio.rest import Client
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -15,46 +13,13 @@ ssm = boto3.client("ssm")
 bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
 dynamodb = boto3.resource("dynamodb")
 
-
-TWILIO_AUTH_TOKEN = ssm.get_parameter(
-    Name="/mainu/twilio/auth",
-    WithDecryption=True
-)["Parameter"]["Value"]
-
-TWILIO_ACCOUNT_SID = ssm.get_parameter(
-    Name="/mainu/twilio/account_sid",
-    WithDecryption=True
-)["Parameter"]["Value"]
-
-
-TWILIO_WHATSAPP_NUMBER= ssm.get_parameter(
-    Name="/mainu/twilio/number",
-    WithDecryption=True
-)["Parameter"]["Value"]
-
-IDEMPOTENCY_TABLE = ssm.get_parameter(
-    Name="/mainu/twilio/idempotency_table"
-)["Parameter"]["Value"]
-table = dynamodb.Table(IDEMPOTENCY_TABLE)
-
 LLM_MODEL = "global.anthropic.claude-haiku-4-5-20251001-v1:0"
 EMBEDDING_MODEL = "amazon.titan-embed-text-v2:0"
 
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
-
-def send_typing_indicator(to: str):
-    """Envia o 'digitando...' para o usuário via Twilio."""
-    try:
-        client.messages.create(
-            to=to,
-            from_=TWILIO_WHATSAPP_NUMBER,
-            persistent_action=["typing_on"],
-            body=""
-        )
-    except Exception as e:
-        print(f"[typing_indicator] Falhou para {to}: {e}")
-
+# IDEMPOTENCY_TABLE = ssm.get_parameter(
+#     Name="/mainu/twilio/idempotency_table"
+# )["Parameter"]["Value"]
+# table = dynamodb.Table(IDEMPOTENCY_TABLE)
 
 def generate_answer(incoming_text):
     response = bedrock_client.converse(
@@ -84,19 +49,24 @@ def generate_answer(incoming_text):
     return response_text
     
 def handler(event, context):
-    print("--- WORKER INICIADO e atualizado via CI/CD---")
     print(f"Evento recebido do SQS: {json.dumps(event)}")
     
     # Verifica se o evento veio mesmo do SQS e tem registros
     if 'Records' in event:
         print(f"Total de mensagens recebidas neste lote: {len(event['Records'])}")
         
+        # message_body = json.dumps({
+        #     "visitorId": visitor_id,
+        #     'sessionId': session_id,
+        #     'environmentId': environment_id,        
+        #     "content": content
+        # })
+
         for record in event['Records']:
-            message_id = record.get('messageId')
             body = json.loads(record.get('body'))
 
-            incoming_text = body["content"]
-            user_number = body["from"]
+            message_id = body['messageId']
+            content = body['content']
 
             timestamp = datetime.now(timezone.utc).isoformat()
             expires_at = int(
@@ -108,7 +78,7 @@ def handler(event, context):
             print(f"Conteúdo do Body: {body}")
 
             item = {
-                "messageSid": message_id,
+                "messageId": message_id,
                 "status": "PROCESSING",
                 "createdAt": timestamp,
                 "expiresAt": expires_at
@@ -119,50 +89,48 @@ def handler(event, context):
             logger.info(type(item["status"]))
             logger.info(type(item["createdAt"]))
             logger.info(type(item["expiresAt"]))
+            logger.info(content)
 
 
             # Verifica se mensagem já foi processada
-            try:
-                table.put_item(
-                    Item=item,
-                    ConditionExpression=Attr("messageSid").not_exists()
-                )
+            # try:
+            #     table.put_item(
+            #         Item=item,
+            #         ConditionExpression=Attr("messageSid").not_exists()
+            #     )
 
-                # We successfully claimed this message
-                send_typing_indicator(user_number)
+            #     response_text = generate_answer(incoming_text)
 
-                response_text = generate_answer(incoming_text)
+            #     completion_timestamp = datetime.now(timezone.utc).isoformat()
 
-                completion_timestamp = datetime.now(timezone.utc).isoformat()
+            #     table.update_item(
+            #         Key={"messageSid": message_id},
+            #         UpdateExpression="SET #s = :s, generatedResponse = :r, completedAt = :t",
+            #         ExpressionAttributeNames={
+            #             "#s": "status"
+            #         },
+            #         ExpressionAttributeValues={
+            #             ":s": "COMPLETED",
+            #             ":r": response_text,
+            #             ":t": completion_timestamp
+            #         }
+            #     )
 
-                table.update_item(
-                    Key={"messageSid": message_id},
-                    UpdateExpression="SET #s = :s, generatedResponse = :r, completedAt = :t",
-                    ExpressionAttributeNames={
-                        "#s": "status"
-                    },
-                    ExpressionAttributeValues={
-                        ":s": "COMPLETED",
-                        ":r": response_text,
-                        ":t": completion_timestamp
-                    }
-                )
+            #     response_message = client.messages.create(
+            #         body=response_text,
+            #         from_=TWILIO_WHATSAPP_NUMBER,
+            #         to=user_number
+            #     )
 
-                response_message = client.messages.create(
-                    body=response_text,
-                    from_=TWILIO_WHATSAPP_NUMBER,
-                    to=user_number
-                )
+            #     logger.info(f'body:{response_text} from: {TWILIO_WHATSAPP_NUMBER} to: {user_number}')
 
-                logger.info(f'body:{response_text} from: {TWILIO_WHATSAPP_NUMBER} to: {user_number}')
+            #     print("Message sent:", response_message.sid)
 
-                print("Message sent:", response_message.sid)
-
-            except ClientError as e:
-                if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
-                    print("Already being processed or already processed")
-                    return
-                raise
+            # except ClientError as e:
+            #     if e.response["Error"]["Code"] == "ConditionalCheckFailedException":
+            #         print("Already being processed or already processed")
+            #         return
+            #     raise
 
     else:
         print("Nenhum registro SQS encontrado no evento.")
